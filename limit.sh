a2a12499607b5186689905567bf9f079af577c02
malayaacx01 <111335173+malayaacx01@users.noreply.github.com>
2024-12-05 02:14:50 +0800

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -1,13 +1,13 @@
 REPO="https://raw.githubusercontent.com/malayaacx01/malayaacx/main/"
-wget -q -O /etc/systemd/system/limitssh.service "${REPO}/limitssh.service" && chmod +x limitssh.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitvmess.service "${REPO}/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitvless.service "${REPO}/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limittrojan.service "${REPO}/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vmess "${REPO}/vmess" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vless "${REPO}/vless" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.trojan "${REPO}/trojan" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.shadowsocks "${REPO}/shadowsocks" >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitssh.service "${REPO}limitssh.service" && chmod +x limitssh.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvmess.service "${REPO}limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvless.service "${REPO}limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limittrojan.service "${REPO}limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vmess "${REPO}vmess" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vless "${REPO}vless" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.trojan "${REPO}trojan" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.shadowsocks "${REPO}shadowsocks" >/dev/null 2>&1
 chmod +x /etc/ssh/limit.ssh
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
