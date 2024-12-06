48b8a10b0617bd1030f7549a2a7d44ea21193812
malayaacx01 <111335173+malayaacx01@users.noreply.github.com>
2024-12-06 10:07:45 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -18,7 +18,7 @@ clear
 #	 menu
 #else
 
-echo -e "Installing TCP BBR Mod By Julak Bantur"
+echo -e "Installing TCP BBR Mod By Malayaacx01 - Shop"
 echo -e "Please Wait BBR Installation Will Starting . . ."
 sleep 5
 clear
